1654513028d797f0f551c9c1a3623128a6580ee8
Juan Bono <juanbono94@gmail.com>
2024-01-24 00:26:15 -0300

    avoid showing the respond_to field in message Debug impl

--- src/lib/raft/message.rs
+++ src/lib/raft/message.rs
@@ -1,7 +1,6 @@
 use super::state::RaftStateType;
 use tokio::sync::oneshot;
 
-#[derive(Debug)]
 pub enum RaftMessage {
     AppendEntries {
         respond_to: oneshot::Sender<bool>,
@@ -27,4 +26,43 @@ pub enum RaftMessage {
     GetRaftStateType {
         respond_to: oneshot::Sender<RaftStateType>,
     },
+}
+
+impl std::fmt::Debug for RaftMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::AppendEntries {
+                term,
+                leader_id,
+                prev_log_index,
+                prev_log_term,
+                entries,
+                leader_commit,
+                ..
+            } => f
+                .debug_struct("AppendEntries")
+                .field("term", term)
+                .field("leader_id", leader_id)
+                .field("prev_log_index", prev_log_index)
+                .field("prev_log_term", prev_log_term)
+                .field("entries", entries)
+                .field("leader_commit", leader_commit)
+                .finish(),
+            Self::RequestVote {
+                term,
+                candidate_id,
+                last_log_index,
+                last_log_term,
+                ..
+            } => f
+                .debug_struct("RequestVote")
+                .field("term", term)
+                .field("candidate_id", candidate_id)
+                .field("last_log_index", last_log_index)
+                .field("last_log_term", last_log_term)
+                .finish(),
+            Self::Timeout => write!(f, "Timeout"),
+            Self::GetRaftStateType { .. } => f.debug_struct("GetRaftStateType").finish(),
+        }
+    }
 }
